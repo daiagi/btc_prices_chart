6cf11a6e6fc7b2e46be84ba52ad18da239d439eb
daiagi <daiagi@gmail.com>
2018-01-10 09:30:48 +0200

    collected static

---
+++ staticfiles/js/chart.js
@@ -0,0 +1,48 @@
+$('document').ready( function() {
+  $.ajax({
+
+    // The URL for the request
+    url: 'http://127.0.0.1:8000/json',
+
+    // The data to send (will be converted to a query string)
+    data: {
+        hours: 1
+    },
+
+    // Whether this is a POST or GET request
+    type: "GET",
+
+    // The type of data we expect back
+    dataType : "json",
+
+
+    success:draw
+
+  });
+});
+
+
+
+
+function draw(data){
+
+  var strictIsoParse = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
+
+  var lineFunction = d3.line()
+                             .x(function(d) { return strictIsoParse(d.time); })
+                             .y(function(d) { return d.global_price; });
+
+   //The SVG Container
+   var svgContainer = d3.select("body").append("svg")
+                                       .attr("width", 200)
+                                       .attr("height", 200);
+
+  svgContainer.append("path")
+                              .attr("d", lineFunction(data))
+                              .attr("stroke", "blue")
+                              .attr("stroke-width", 2)
+                              .attr("fill", "none");
+
+
+
+};
