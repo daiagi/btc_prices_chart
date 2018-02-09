74fa3949ef19161eaba526e9dd6a0a47381b9d6a
daiagi <daiagi@gmail.com>
2018-02-09 11:55:21 +0200

    changes to price_db

--- database/price_db.py
+++ database/price_db.py
@@ -6,14 +6,7 @@ class priceDB:
         heroku_conn = hk3.from_key('63d29faf-cd0d-41cc-a737-63171d4bb83c')
         app = heroku_conn.apps()[0]
         self.db_url = app.config()['DATABASE_URL']
-        #testing
-        print(pg.connect(self.db_url))
 
-    # host = 'ec2-50-19-126-219.compute-1.amazonaws.com'
-    # user = 'lcavnxrgimumrk'
-    # passw = '7d670028ab713e7cbad242954e778a7e01a3d5dff758a27a1730eb8555be31ec'
-    # db_name = 'd2pqk4jrbaclte'
-    # table_name = 'btc_price'
 
     def _execute(self,sql_command,params = None):
         conn =  pg.connect(self.db_url)
