b28763fd443584635a024a2963f560041e8ce383
daiagi <daiagi@gmail.com>
2018-02-09 14:12:37 +0200

    v

--- database/test_connection.py
+++ database/test_connection.py
@@ -14,8 +14,8 @@ conn = psycopg2.connect(
     host=url.hostname,
     port=url.port
 )
-command = """INSERT INTO {TABLE_NAME} (time,bit2c_price_ils,global_price_ils,bit2c_price_usd,global_price_usd)
-                    VALUES (%s,%s,%s,%s,%s);""".format('btc_price')
+command = """INSERT INTO btc_price (time,bit2c_price_ils,global_price_ils,bit2c_price_usd,global_price_usd)
+                    VALUES (%s,%s,%s,%s,%s);"""
 
 with conn.cursor() as cur:
 	cur.execute(command,datetime.utcnow(),1,2,3,4)
