4916130156d4e7652fd3f071d69e9e12fa97e682
Baruch <daiagi@gmail.com>
2018-03-04 11:10:58 +0200

    removed a query field in the DB request to improve performance

--- btc_chart/views.py
+++ btc_chart/views.py
@@ -33,7 +33,7 @@ def getBtcPrice_view(request):
 
     query = BtcPrice.objects.filter(time__gt = now_utc- time_range ).only(
     'bit2c_price_ils','global_price_ils',
-    'bit2c_price_usd','global_price_usd').values()
+    'global_price_usd').values()
 
     # def del_time(dict):
     #     del dict['time']
